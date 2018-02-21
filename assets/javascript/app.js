42a38d0bed64155485abca74b60fabb3b309eedc
Andrew Johnson <32281250+andrewj12763@users.noreply.github.com>
2018-02-20 19:13:27 -0700

    adding function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,45 @@
-$(document).ready(function() {
+function check(){
 
-var number = 30;
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+	var question3 = document.quiz.question3.value;
+	var correct = 0;
+
+
+	if (question1 == "Andrew Johnson") {
+		correct++;
+}
+	if (question2 == "Black") {
+		correct++;
+}	
+	if (question3 == "Gimley") {
+		correct++;
+	}
+	
+	var pictures = ["img/win.gif", "img/meh.jpeg", "img/lose.gif"];
+	var messages = ["Great job!", "That's just okay", "How Sad!"];
+	var score;
+
+	if (correct == 0) {
+		score = 2;
+	}
+
+	if (correct > 0 && correct < 3) {
+		score = 1;
+	}
+
+	if (correct == 3) {
+		score = 0;
+	}
+
+	document.getElementById("after_submit").style.visibility = "visible";
+
+	document.getElementById("message").innerHTML = messages[score];
+	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+	document.getElementById("picture").src = pictures[score];
+	}
+
+	var number = 30;
 var intervalId;
 
     $("#stop").on("click", stop);
@@ -27,6 +66,4 @@ function results(){
 	if(number == 0){
 		// results page
 	}
-}		
-
-});
+}

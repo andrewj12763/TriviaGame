d6dc5cfebf6248e7c841525dcdcbd91f22c0d476
Andrew Johnson <32281250+andrewj12763@users.noreply.github.com>
2018-02-21 20:21:12 -0700

    final function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,69 +1,65 @@
+// results
 function check(){
 
 	var question1 = document.quiz.question1.value;
 	var question2 = document.quiz.question2.value;
 	var question3 = document.quiz.question3.value;
 	var correct = 0;
-
-
-	if (question1 == "Andrew Johnson") {
-		correct++;
-}
-	if (question2 == "Black") {
-		correct++;
-}	
-	if (question3 == "Gimley") {
-		correct++;
-	}
-	
-	var pictures = ["img/win.gif", "img/meh.jpeg", "img/lose.gif"];
 	var messages = ["Great job!", "That's just okay", "How Sad!"];
 	var score;
 
+	if (question1 == "Andrew Johnson") {
+		correct++;
+	}
+	if (question2 == "Black") {
+		correct++;
+	}	
+	if (question3 == "Gimley") {
+		correct++;
+	}
 	if (correct == 0) {
 		score = 2;
 	}
-
 	if (correct > 0 && correct < 3) {
 		score = 1;
 	}
-
 	if (correct == 3) {
 		score = 0;
 	}
-
+// make results visible after sumbit.
+//  I found this way of using vanilla JS that seemed the more optimal way of using the form tags
 	document.getElementById("after_submit").style.visibility = "visible";
-
 	document.getElementById("message").innerHTML = messages[score];
 	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
-	document.getElementById("picture").src = pictures[score];
-	}
-
-	var number = 30;
-var intervalId;
-
-    $("#stop").on("click", stop);
-    $("#resume").on("click", run);
-
-    function run() {
-      clearInterval(intervalId);
-      intervalId = setInterval(decrement, 1000);
-    }
-    function decrement() {
-      number--;
-      $("#show-number").html("<h2>Time Remaining: " + number + "</h2>");
-      if (number === 0) {
-        stop();
-        number = 100;
-      }
-    }
-    function stop() {
-      clearInterval(intervalId);
-    }
-		run();
-		
-function results(){
-	if(number == 0){
-		// results page
-	}
 }
+
+// Timer
+	var number = 30;
+	var intervalId;
+
+  $("#stop").on("click", stop);
+  $("#resume").on("click", run);
+
+function run() {
+   clearInterval(intervalId);
+   intervalId = setInterval(decrement, 1000);
+    }
+function decrement() {
+   number--;
+   $("#show-number").html("<h2>Time Remaining: " + number + "</h2>");
+  if (number === 0) {
+		 stop();
+		 check();
+		 number = 100;
+			$("#button").hide( 2000, function() {
+				$( this ).remove();
+			});
+     }
+		}
+	$('#button').on('click', function(){
+		stop()
+	})
+function stop() {
+    clearInterval(intervalId);
+		}
+		run();
